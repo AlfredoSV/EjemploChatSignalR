107829072200086365d7d394592937215a88ceab
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-28 20:27:46 -0600

    Agregar archivos de proyecto.

---
+++ EjemploChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploChatSignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async void EnviarMensaje(string usuario, string mensaje)
+        {
+            await Clients.All.SendAsync("RecibirMensaje", usuario, mensaje);
+        }
+    }
+}
---
+++ EjemploChatSignalR/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chathub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+// Start the connection.
+start();
+
+connection.on("RecibirMensaje", (usuario, mensaje) => {
+
+    let divMensajes = document.querySelector("#mensajes");
+    divMensajes.innerHTML += "<div><p>" + usuario + ":" + mensaje + "</p></div>"
+  
+});
+
+document.querySelector("#btnEnviarMensaje").addEventListener("click", (e) => {
+
+    let mensaje = document.querySelector("#mensaje").value;
+    let usuario = document.querySelector("#usuario").value;
+
+    connection.invoke("EnviarMensaje", usuario, mensaje).catch(err => console.log(err));
+
+    e.preventDefault();
+});
